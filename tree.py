60ed01807ac1153590860217c11a25bd8b42739d
ervinbz <88996576+ervinbz@users.noreply.github.com>
2023-04-15 21:07:15 -0400

    Delete tree.py

--- tree.py
+++
@@ -1,8 +0,0 @@
-class Node:
-    def __init__(self, data, left=None, right=None):
-        self.data = data
-        self.left = left
-        self.right = right
-
-    def is_leaf(self):
-        return self.left is None and self.right is None
